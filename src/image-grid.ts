6d73749fdf7583815dcb9c09ae9163796aff2a55
FAL <falworks.contact@gmail.com>
2019-12-06 22:25:29 +0900

    add outerMargin parameter

--- src/image-grid.ts
+++ src/image-grid.ts
@@ -6,17 +6,20 @@ export const create = (
   images: readonly p5.Image[] | readonly p5.Element[],
   parameters: Parameters.Unit
 ) => {
-  const { width, height, rows, columns } = parameters;
+  const { width, height, rows, columns, outerMargin } = parameters;
   const imageCount = images.length;
 
-  const cellWidth = width / columns;
-  const cellHeight = height / rows;
+  const cellWidth = (width - outerMargin.left - outerMargin.right) / columns;
+  const cellHeight = (height - outerMargin.top - outerMargin.bottom) / rows;
   let imageIndex = 0;
 
   const graphics = p.createGraphics(width, height);
   graphics.push();
   graphics.imageMode(p.CENTER);
-  graphics.translate(cellWidth / 2, cellHeight / 2);
+  graphics.translate(
+    outerMargin.left + cellWidth / 2,
+    outerMargin.top + cellHeight / 2
+  );
 
   for (let row = 0; row < rows; row += 1) {
     const y = row * cellHeight;
--- src/parameters.ts
+++ src/parameters.ts
@@ -3,13 +3,25 @@ export type Unit = {
   readonly height: number;
   readonly columns: number;
   readonly rows: number;
+  readonly outerMargin: {
+    readonly top: number;
+    readonly bottom: number;
+    readonly left: number;
+    readonly right: number;
+  };
 };
 
 export const defaultValues: Unit = {
   width: 800,
   height: 800,
   columns: 2,
-  rows: 2
+  rows: 2,
+  outerMargin: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  }
 };
 
 export const defaultString = JSON.stringify(defaultValues, undefined, 2);
@@ -32,5 +44,14 @@ export const parse = (jsonString: string): Unit => {
   const columns = validateNumber(parsed.columns, defaultValues.columns);
   const rows = validateNumber(parsed.rows, defaultValues.rows);
 
-  return { width, height, columns, rows };
+  const parsedOuterMargin = parsed.outerMargin;
+  const defaultOuterMargin = defaultValues.outerMargin;
+  const outerMargin = {
+    top: validateNumber(parsedOuterMargin.top, defaultOuterMargin.top),
+    bottom: validateNumber(parsedOuterMargin.bottom, defaultOuterMargin.bottom),
+    left: validateNumber(parsedOuterMargin.left, defaultOuterMargin.left),
+    right: validateNumber(parsedOuterMargin.right, defaultOuterMargin.right)
+  };
+
+  return { width, height, columns, rows, outerMargin };
 };
