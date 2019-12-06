f395e019cf66ad14d366dba7a9b0975a86e77015
FAL <falworks.contact@gmail.com>
2019-12-06 22:13:31 +0900

    improve parsing parameters

--- src/parameters.ts
+++ src/parameters.ts
@@ -1,8 +1,8 @@
 export type Unit = {
-  width: number;
-  height: number;
-  columns: number;
-  rows: number;
+  readonly width: number;
+  readonly height: number;
+  readonly columns: number;
+  readonly rows: number;
 };
 
 export const defaultValues: Unit = {
@@ -15,7 +15,22 @@ export const defaultValues: Unit = {
 export const defaultString = JSON.stringify(defaultValues, undefined, 2);
 
 const reviver = (key: string, value: any) =>
-  key === "" || Number.isFinite(value) ? value : undefined;
+  key === "" ||
+  (typeof value === "object" && !Array.isArray(value)) ||
+  Number.isFinite(value)
+    ? value
+    : undefined;
 
-export const parse = (jsonString: string): Unit =>
-  Object.assign(Object.create(defaultValues), JSON.parse(jsonString, reviver));
+const validateNumber = (value: any, defaultValue: number) =>
+  Number.isFinite(value) ? value : defaultValue;
+
+export const parse = (jsonString: string): Unit => {
+  const parsed = JSON.parse(jsonString, reviver);
+
+  const width = validateNumber(parsed.width, defaultValues.width);
+  const height = validateNumber(parsed.height, defaultValues.height);
+  const columns = validateNumber(parsed.columns, defaultValues.columns);
+  const rows = validateNumber(parsed.rows, defaultValues.rows);
+
+  return { width, height, columns, rows };
+};
