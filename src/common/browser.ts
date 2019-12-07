197a51eb14d599d976c42e4be3ec9480d7c9e964
FAL <falworks.contact@gmail.com>
2019-12-08 02:17:53 +0900

    fix Browser.determine

--- src/common/browser.ts
+++ src/common/browser.ts
@@ -11,13 +11,13 @@ export enum Type {
 export const determine = () => {
   const userAgent = window.navigator.userAgent.toLowerCase();
 
-  if (userAgent.indexOf("msie") != -1 || userAgent.indexOf("trident") != -1)
+  if (userAgent.indexOf("msie") !== -1 || userAgent.indexOf("trident") !== -1)
     return Type.IE;
-  if (userAgent.indexOf("edge") != -1) return Type.Edge;
-  if (userAgent.indexOf("chrome") != -1) return Type.Chrome;
-  if (userAgent.indexOf("safari") != -1) return Type.Safari;
-  if (userAgent.indexOf("firefox") != -1) return Type.FireFox;
-  if (userAgent.indexOf("opera") != -1) return Type.Opera;
+  if (userAgent.indexOf("edge") !== -1) return Type.Edge;
+  if (userAgent.indexOf("chrome") !== -1) return Type.Chrome;
+  if (userAgent.indexOf("safari") !== -1) return Type.Safari;
+  if (userAgent.indexOf("firefox") !== -1) return Type.FireFox;
+  if (userAgent.indexOf("opera") !== -1) return Type.Opera;
 
-  return Type.Opera;
+  return Type.Other;
 };
