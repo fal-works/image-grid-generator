90d8fb49c18621d4a501d93dada708752c546b67
FAL <falworks.contact@gmail.com>
2019-12-07 22:27:42 +0900

    add Browser module

---
+++ src/common/browser.ts
@@ -0,0 +1,23 @@
+export enum Type {
+  IE,
+  Edge,
+  Chrome,
+  Safari,
+  FireFox,
+  Opera,
+  Other
+}
+
+export const determine = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+
+  if (userAgent.indexOf("msie") != -1 || userAgent.indexOf("trident") != -1)
+    return Type.IE;
+  if (userAgent.indexOf("edge") != -1) return Type.Edge;
+  if (userAgent.indexOf("chrome") != -1) return Type.Chrome;
+  if (userAgent.indexOf("safari") != -1) return Type.Safari;
+  if (userAgent.indexOf("firefox") != -1) return Type.FireFox;
+  if (userAgent.indexOf("opera") != -1) return Type.Opera;
+
+  return Type.Opera;
+};
